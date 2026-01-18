8fb007596ace3610e095ceceeb04b5585c234915
Soleil92 <soleilmwiza@gmail.com>
2026-01-18 18:58:02 +0100

    Update Ci

--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -1,16 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from "react-router-dom";
+
 import { Container,
            Box,
            Typography,
            TextField,
-           ButtonGroup,
-           Button,
            Stack,} from "@mui/material";
 
 export default function ChatRoom() {
-     const [email, setEmail]=useState('');
-     const [userName, setUserName]=useState('');
     return (
         <Container maxWidth="sm" sx={{ mt: 4 }}>
               <Box sx={{ p: 4, boxShadow: 3, borderRadius: 2 }}>
@@ -21,7 +16,6 @@ export default function ChatRoom() {
                 <Stack spacing={2} mt={2}>
           <TextField
                               label="Email"
-                              value={email}
                               fullWidth
                             />
         <TextField fullWidth label="New message" id="fullWidth" />
